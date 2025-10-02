b301e253bb9106902af9e67f1612595fdbb55f99
daisytech999 <disha.j.zala@gmail.com>
2025-10-02 01:14:56 -0400

    commit css changes

--- src/components/ChevronButton.jsx
+++ src/components/ChevronButton.jsx
@@ -8,8 +8,23 @@ function ChevronButton({ isActive, onClick }) {
     <button
       className={`chevron ${isActive ? "active" : ""}`}
       onClick={onClick}
+      aria-label="Select product"
     >
-     ⮜
+      {/* SVG Arrow */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon"
+      >
+        <polyline points="9 18 15 12 9 6"></polyline>
+      </svg>
     </button>
   );
 }
